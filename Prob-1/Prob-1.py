7c07ae93dae91f719742bb0708670e173e33b1db
rachel watson <r.watson7@students.clark.edu>
2019-05-04 12:34:54 -0700

    Final Submission_Rachel_Watson

--- Prob-1/Prob-1.py
+++ Prob-1/Prob-1.py
@@ -4,13 +4,12 @@
 
 # Rachel Watson
 
-def shippingCost(subtotal):
-
+def shippingCost(orderSubTotal):
     shippingCost = 2.99
 
     # enter code here to test for free
-    if subtotal >= 10.00:
-        total= subtotal + shippingCost
+    if orderSubTotal >= 10.00:
+        shippingCost = 0.0
    
     return shippingCost
 
@@ -18,9 +17,9 @@ def shippingCost(subtotal):
 
 def unitTest():
 
-    print("Shipping cost if subtotal < 10.00:\t", shippingCost(5.99))
+    print("Shipping cost if subtotal < 10.00:\t", shippingCost(9.99))
     # enter additional test code here
-
-
+    print("Shipping cost if subtotal = 10.00:\t", shippingCost(10.00))
+    print("Shipping cost if subtotal > 10.00:\t", shippingCost(10.01))
 if __name__ == "__main__":
     unitTest()
